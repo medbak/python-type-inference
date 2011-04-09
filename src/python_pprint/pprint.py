96970a49d6f8c64e2d94bb277ffa0762ebafc174
soonho.kong@gmail.com <soonho.kong@gmail.com@e4acdad2-aaf9-b8b8-a053-ad403d3a3084>
2011-04-09 19:29:20 +0000

    Use Python AST module's dump method to pretty-print. This allows us to have a location information (line number & column number) for each program construct. For example, the following Python code ======================= x = 3 y = 5 z = x + y a = 3.1415 b = 8.14189 c = a + b k1 = 3L k2 = 3412L k3 = k1+k2 m1 = -123124 m2 = -124897235.2413 m4 = m5 = m3 = m1 - m2 ======================= will be printed as ======================= Module([Assign([Name('x', Store(), lineno=1, col_offset=0)], Num(3, lineno=1, col_offset=4), lineno=1, col_offset=0), Assign([Name('y', Store(), lineno=2, col_offset=0)], Num(5, lineno=2, col_offset=4), lineno=2, col_offset=0), Assign([Name('z', Store(), lineno=3, col_offset=0)], BinOp(Name('x', Load(), lineno=3, col_offset=4), Add(), Name('y', Load(), lineno=3, col_offset=8), lineno=3, col_offset=4), lineno=3, col_offset=0), Assign([Name('a', Store(), lineno=4, col_offset=0)], Num(3.1415, lineno=4, col_offset=4), lineno=4, col_offset=0), Assign([Name('b', Store(), lineno=5, col_offset=0)], Num(8.14189, lineno=5, col_offset=4), lineno=5, col_offset=0), Assign([Name('c', Store(), lineno=6, col_offset=0)], BinOp(Name('a', Load(), lineno=6, col_offset=4), Add(), Name('b', Load(), lineno=6, col_offset=8), lineno=6, col_offset=4), lineno=6, col_offset=0), Assign([Name('k1', Store(), lineno=7, col_offset=0)], Num(3L, lineno=7, col_offset=5), lineno=7, col_offset=0), Assign([Name('k2', Store(), lineno=8, col_offset=0)], Num(3412L, lineno=8, col_offset=5), lineno=8, col_offset=0), Assign([Name('k3', Store(), lineno=9, col_offset=0)], BinOp(Name('k1', Load(), lineno=9, col_offset=5), Add(), Name('k2', Load(), lineno=9, col_offset=8), lineno=9, col_offset=5), lineno=9, col_offset=0), Assign([Name('m1', Store(), lineno=10, col_offset=0)], Num(-123124, lineno=10, col_offset=6), lineno=10, col_offset=0), Assign([Name('m2', Store(), lineno=11, col_offset=0)], Num(-124897235.2413, lineno=11, col_offset=6), lineno=11, col_offset=0), Assign([Name('m4', Store(), lineno=12, col_offset=0), Name('m5', Store(), lineno=12, col_offset=5), Name('m3', Store(), lineno=12, col_offset=10)], BinOp(Name('m1', Load(), lineno=12, col_offset=15), Sub(), Name('m2', Load(), lineno=12, col_offset=20), lineno=12, col_offset=15), lineno=12, col_offset=0)]) =======================

--- src/python_pprint/pprint.py
+++ src/python_pprint/pprint.py
@@ -1,39 +1,9 @@
 #!/usr/bin/env python
 """Python AST pretty-printer."""
 
+import ast
+import codegen
 import sys
-from compiler.ast import Node
-
-def pprintAst(ast, indent='  ', stream=sys.stdout):
-    """Pretty-print an AST to the given output stream."""
-    rec_node(ast, 0, indent, stream.write)
-
-def rec_node(node, level, indent, write):
-    """Recurse through a node, pretty-printing it."""
-    pfx = indent * level
-    if isinstance(node, Node):
-        write(pfx)
-        write(node.__class__.__name__)
-        write('(')
-
-        if any(isinstance(child, Node) for child in node.getChildren()):
-            for i, child in enumerate(node.getChildren()):
-                if i != 0:
-                    write(',')
-                write('\n')
-                rec_node(child, level+1, indent, write)
-            write('\n')
-            write(pfx)
-        else:
-            # None of the children as nodes, simply join their repr on a single
-            # line.
-            write(', '.join(repr(child) for child in node.getChildren()))
-
-        write(')')
-
-    else:
-        write(pfx)
-        write(repr(node))
 
 def usage():
 	print "Print Python AST:"
@@ -44,6 +14,8 @@ if __name__ == '__main__':
 	if(len(sys.argv) != 2):
 		usage()
 	else:
-      		import compiler
-        	pprintAst(compiler.parseFile(sys.argv[1]))
+            src = "".join(file(sys.argv[1]).readlines())
+            root = ast.parse(src)
+            # Do not annotate field, but print line number & column info
+            print ast.dump(root, False, True)
     main()
