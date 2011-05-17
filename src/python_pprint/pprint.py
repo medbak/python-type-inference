d5f125a66dd5afc71136f247819b847f0818757a
soonho.kong@gmail.com <soonho.kong@gmail.com@e4acdad2-aaf9-b8b8-a053-ad403d3a3084>
2011-05-17 13:13:33 +0000

    add more pattern matching...

--- src/python_pprint/pprint.py
+++ src/python_pprint/pprint.py
@@ -4,18 +4,41 @@
 import ast
 import codegen
 import sys
+import getopt
 
 def usage():
-	print "Print Python AST:"
-	print "Usage: ./" + __file__ + " <.py>"
+	print "Print Python AST"
+	print "Usage   : " + __file__, "[option]", "<.py>"
+	print "Option  :  -l = print line number & column info"
 
-if __name__ == '__main__':
-    def main():
-	if(len(sys.argv) != 2):
-		usage()
-	else:
-            src = "".join(file(sys.argv[1]).readlines())
-            root = ast.parse(src)
-            # Do not annotate field, but print line number & column info
-            print ast.dump(root, False, True)
+def pprint(filename, linenumber):
+        src = "".join(file(filename).readlines())
+        root = ast.parse(src)
+        # Do not annotate field, but print line number & column info
+        print ast.dump(root, False, linenumber)
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hl", ["help", "linenumber"])
+    except getopt.GetoptError, err:
+        # print help information and exit:
+        print str(err) # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    linenumber = False
+    if(len(args) != 1):
+	usage()
+	sys.exit()
+    for o, a in opts:
+        if o == "-l":
+            linenumber = True
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        else:
+            assert False, "unhandled option"
+    pprint(args[0], linenumber)
+
+if __name__ == "__main__":
     main()
+
