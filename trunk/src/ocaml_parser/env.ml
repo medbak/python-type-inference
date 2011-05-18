26ab1d38c684201be8040c6b32b3af4cf535fbfd
soonho.kong@gmail.com <soonho.kong@gmail.com@e4acdad2-aaf9-b8b8-a053-ad403d3a3084>
2011-05-18 16:28:29 +0000

    env.ml : add environment join type.ml : add norm function for list of types analysis.ml : cover dict, list, tuple, set, list comp, set comp, dict comp

--- trunk/src/ocaml_parser/env.ml
+++ trunk/src/ocaml_parser/env.ml
@@ -3,4 +3,16 @@ open Ast
 open Type
 
 let empty_env = PMap.empty
-let join env1 env2 = env1
+let join env1 env2 =
+  PMap.foldi
+    (fun key value1 env ->
+      try
+        let value2 = PMap.find key env in
+        let join_result = Type.join [value1; value2] in
+        PMap.add key join_result env
+      with
+          Not_found ->
+            PMap.add key value1 env
+    )
+    env1
+    env2
