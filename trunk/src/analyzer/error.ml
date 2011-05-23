a0eb85e5f45c6a8cf161314a5e2ab38a767720db
soonho.kong@gmail.com <soonho.kong@gmail.com@e4acdad2-aaf9-b8b8-a053-ad403d3a3084>
2011-05-23 14:55:19 +0000

    1) add yiled and generator expression. 2) TypeError reports location information as well

--- trunk/src/analyzer/error.ml
+++ trunk/src/analyzer/error.ml
@@ -12,17 +12,17 @@ let decr_ln () = linenum := !linenum - 1
 let get_ln () = !linenum
 
 let init () = linenum := 1
-
+  
 let handle_exn v =
-	match v with
+  match v with
 	  Lex_err (s, i) ->
-	  Printf.eprintf ">> lexical error at line %d: %s\n" i s
+	    Printf.eprintf ">> lexical error at line %d: %s\n" i s
 	| Parsing.Parse_error ->
 	  Printf.eprintf ">> syntax error at line %d\n" !linenum
 	| Arg.Bad s ->
 	  Printf.eprintf ">> file format error: %s\n" s
 	| Type.RuntimeError s ->
 	  Printf.eprintf ">> runtime error: %s\n" s
-	| Type.TypeError s ->
-	  Printf.eprintf ">> type error: %s\n" s
+	| Type.TypeError (s, (line, col)) ->
+	  Printf.eprintf ">> type error: %s at (%d, %d)\n" s line col
 	|  _ -> raise v
