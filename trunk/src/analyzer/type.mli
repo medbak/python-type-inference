e6838c46a70e81315d5cd7bb6c697338b15f8a1d
soonho.kong@gmail.com <soonho.kong@gmail.com@e4acdad2-aaf9-b8b8-a053-ad403d3a3084>
2011-05-26 21:45:53 +0000

    add more descriptions in the interface files (*.mli) run "make doc" to have documentation HTML files.

--- trunk/src/analyzer/type.mli
+++ trunk/src/analyzer/type.mli
@@ -1,50 +1,73 @@
-(*
- * Soonho Kong (soonhok@cs.cmu.edu)
- *
- * type.ml : Python Type Definition
- * Reference : http://docs.python.org/reference/datamodel.html#the-standard-type-hierarchy
- *)
-exception NotImplemented
+(** Python Types
+    @see <http://docs.python.org/reference/datamodel.html#the-standard-type-hierarchy> Python Standard Type Hierarchy
+    
+    @author Soonho Kong (soonhok at cs.cmu.edu)
+*)
+    
+(** {6 Types} *)
+    
 type ty =
-    TyBot
-  | TyTop
+    TyBot                                    (** Bottom *)
+  | TyTop                                    (** Top *)
   | TyNone
   | TyVar of string * Ast.loc * int * ty
+  (** Type variable is introduce at the function definition. TyVar
+      (function name, location, position, constraint) *)
   | TyNotImplemented
   | TyEllipsis
-  | TyInt
-  | TyLong
-  | TyBool
-  | TyFloat
-  | TyComplex
-  | TyString of int
-  | TyAString
-  | TyUnicode of int
-  | TyAUnicode
-  | TyTuple of ty list
-  | TyATuple of ty
-  | TyList of ty list
-  | TyAList of ty
-  | TyByteArray of int
-  | TyAByteArray
-  | TySet of ty * int         
-  | TyFrozenSet of ty * int   
-  | TyASet of ty              
-  | TyAFrozenSet of ty        
-  | TyDict of (ty * ty) list
-  | TyFunction of (ty list * ty)
-  | TyGenerator of ty * int
-  | TyAGenerator of ty
-  | TyObject
-  | TyType of ty
-  | TyUnion of ty list
-  | TyClass of (string * ty) list
-val normalize : 'a list -> 'a list
-val join : ty list -> ty
-val order_set : ty list -> ty list -> bool
-val order_list : ty list -> ty list -> bool
-val order : ty -> ty -> bool
-val to_string : ty -> string
+  | TyInt           (** Int *)
+  | TyLong          (** Long *)
+  | TyBool          (** Bool *)
+  | TyFloat         (** Float *)
+  | TyComplex       (** Complex *)
+  | TyString of int (** Concrete string type which maintains the length of string *)
+  | TyAString       (** Abstract string type *)
+  | TyUnicode of int (** Concrete unicode type which maintains the length *)
+  | TyAUnicode       (** Abstract unicode type *)
+  | TyTuple of ty list (** Concrete tuple type, which maintains the type of each element *)
+  | TyATuple of ty     (** Abstract tuple type, every element has ty type *)
+  | TyList of ty list  (** Concrete list type, which maintains the type of each element *)
+  | TyAList of ty      (** Abstract list type *)
+  | TyByteArray of int  (** Concrete bytearray type which maintains the length *)
+  | TyAByteArray        (** Abstract bytearray type *)
+  | TySet of ty * int   (** Concrete set type which maintains the size *)
+  | TyASet of ty              (** Abstract set type *)
+  | TyFrozenSet of ty * int   (** Concrete frozenset type which maintains the size *)
+  | TyAFrozenSet of ty        (** Abstract frozenset type *)
+  | TyDict of (ty * ty) list  (** Dict type, maintains (key_type, value_type) list *)
+  | TyFunction of (ty list * ty) (** Function type, (argument_type list, return_type) *)
+  | TyGenerator of ty * int  (** Concrete generator type which maintains a length of its contents *)
+  | TyAGenerator of ty       (** Abstract generator type *)
+  | TyObject                 (** Object type *)
+  | TyType of ty             (** Type type *)
+  | TyUnion of ty list       (** Union type *)
+  | TyClass of (string * ty) list (** Class type. Map from attribute/method names -> their types *)
+
+(** {6 Exceptions} *)
+  
 exception RuntimeError of string
 exception TypeError of string * Ast.loc
 exception TypeWarning of string * Ast.loc
+      
+(** {6 Values} *)
+
+(** [normalize tylist] returns tylist where duplicates are removed. *)
+val normalize : 'a list -> 'a list
+
+(** Order operation. *)  
+val order : ty -> ty -> bool
+
+(** [order_set tylist1 tylist2] is true iff
+    ∀ ty1 ∈ tylist1. ∃ ty2 ∈ tylist2. [order ty1 ty2] *)
+val order_set : ty list -> ty list -> bool
+
+(** [order_list \[ty_11, ty_12, ..., ty_1n\] \[ty_21, ty_22, ..., ty_2n\]
+    ] is true iff [order ty_11 ty_21] & [order ty_12 ty_22] & ... & [order ty_1n ty_2n] *)
+val order_list : ty list -> ty list -> bool
+
+(** Join operation. *)
+val join : ty list -> ty
+
+val to_string : ty -> string
+
+  
