ebfc968981e28b478a6a6cb126d593e425f72a68
soonho.kong@gmail.com <soonho.kong@gmail.com@e4acdad2-aaf9-b8b8-a053-ad403d3a3084>
2011-08-19 02:46:51 +0000

    add

--- src/analyzer/type.ml
+++ src/analyzer/type.ml
@@ -189,19 +189,20 @@ let rec add ty1 ty2 = match (ty1, ty2) with
   | (TyBool, _) -> add TyInt ty2
   | (_, TyBool) -> add ty1 TyInt
   | (TyCInt n, TyCInt m) -> TyCInt (n + m)
+  | (TyCInt n, TyInt) -> TyInt
   | (TyCInt n, _) -> raise NotImplemented
-  | (TyCInt n, TyInt) -> 
   | (TyInt, TyInt)    -> TyInt
   | (TyInt, _) -> raise NotImplemented
   | (TyCLong n, TyCInt m) -> TyCLong (n + m)
   | (TyCLong n, TyCLong m) -> TyCLong (n + m)
+  | (TyCLong n, TyInt) -> TyLong
+  | (TyCLong n, TyLong) -> TyLong
   | (TyCLong n, _) -> raise NotImplemented
-  | (TyCLong n, TyInt)  
-  | (TyCLong n, TyLong)  
-  | (TyLong, TyCInt _)  
-  | (TyLong, TyInt)     
-  | (TyLong, TyCLong _) 
+  | (TyLong, TyCInt _) -> TyLong
+  | (TyLong, TyInt) -> TyLong
+  | (TyLong, TyCLong _) -> TyLong
   | (TyLong, TyLong)    -> TyLong
-  | (TyCFloat n, TyCInt m) -> TyCFloat (n + (float_of_int m))
+  | (TyCFloat n, TyCInt m) -> TyCFloat (n +. (float_of_int m))
   | (TyCFloat n, TyInt) -> TyFloat
+  | (TyCFloat n, TyCLong m) -> TyCFloat (n +. (float_of_int m))
 
